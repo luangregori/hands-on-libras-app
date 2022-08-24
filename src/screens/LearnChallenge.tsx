19fd47984b20b763735366040ad06e8ac2963c37
Luan de Gregori <luan.gregori@banqi.com>
2022-08-24 11:03:47 -0300

    feat: add screen to complete learning

--- src/screens/LearnChallenge.tsx
+++ src/screens/LearnChallenge.tsx
@@ -25,6 +25,7 @@ let webViewRef;
 const LearnChallenge = ({ route, navigation }: Props) => {
   const [learningInfo, setLearningInfo] = useState([]);
   const [stepId, setStepId] = useState('0');
+  const [completed, setCompleted] = useState(false);
   const { challengeId } = route.params;
 
   const _getLearningInfo = async () => {
@@ -66,45 +67,63 @@ const LearnChallenge = ({ route, navigation }: Props) => {
     _getLearningInfo();
   }, []);
 
+  if (!completed) {
+    return (
+      <SafeAreaView style={styles.container}>
+  
+        <StatusBar backgroundColor={theme.colors.backdrop} />
+  
+        <BackButton goBack={() => navigation.navigate('Dashboard')} />
+  
+        <View style={styles.avatar}>
+          <WebView
+            style={{ marginLeft: "-50%" }}
+            width={'200%'}
+            onMessage={(event) => {
+              if (event.nativeEvent.data === "PLAYER_LOADED") {
+                playWord(learningInfo.find(val => val.id === stepId)?.word)
+              }
+            }}
+            ref={(ref) => webViewRef = ref}
+            source={{ uri: 'https://hands-on-libras.web.app/?cc=true' }}
+          />
+        </View>
+  
+        <View style={styles.description}>
+          <Paragraph>
+            {learningInfo.find(val => val.id === stepId)?.description}
+          </Paragraph>
+        </View>
+  
+        <View style={styles.buttons}>
+          {_hasNextStep() ?
+            <Button mode="contained" onPress={_nextStep}>
+              Proximo
+            </Button>
+            :
+            <Button mode="contained"
+              onPress={() => setCompleted(true)}>
+              Finalizar
+            </Button>
+          }
+        </View>
+      </SafeAreaView>
+    );
+  }
+  
   return (
     <SafeAreaView style={styles.container}>
-
       <StatusBar backgroundColor={theme.colors.backdrop} />
-
-      <BackButton goBack={() => navigation.navigate('Dashboard')} />
-
-      <View style={styles.avatar}>
-        <WebView
-          style={{ marginLeft: "-50%" }}
-          width={'200%'}
-          onMessage={(event) => {
-            if (event.nativeEvent.data === "PLAYER_LOADED") {
-              playWord(learningInfo.find(val => val.id === stepId)?.word)
-            }
-          }}
-          ref={(ref) => webViewRef = ref}
-          source={{ uri: 'https://hands-on-libras.web.app/?cc=true' }}
-        />
-      </View>
-
-      <View style={styles.description}>
+      <View style={styles.content}>
+        <Image style={styles.image} source={require('../assets/learn-completed-successfully.png')}/>
         <Paragraph>
-          {learningInfo.find(val => val.id === stepId)?.description}
+          Desafio concluído com sucesso!!
         </Paragraph>
-      </View>
-
-      <View style={styles.buttons}>
-        {_hasNextStep() ?
-          <Button mode="contained" onPress={_nextStep}>
-            Proximo
-          </Button>
-          :
-          <Button mode="contained"
-            //TODO: ir para tela lição aprendida com sucesso
-            onPress={() => navigation.navigate('Dashboard')}>
-            Finalizar
-          </Button>
-        }
+        <Button mode="contained"
+          // TODO: chamar rota que vai dar pontos por concluir o aprendizado
+          onPress={() => navigation.navigate('Dashboard')}>
+          Finalizar
+        </Button>
       </View>
     </SafeAreaView>
   );
@@ -128,13 +147,21 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     fontSize: 14,
     justifyContent: 'center',
-    backgroundColor: 'cyan'
   },
   buttons: {
     marginTop: 10,
     marginHorizontal: 20,
     paddingLeft: '50%',
-  }
+  },
+  content: {
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '50%',
+  },
 });
 
 
